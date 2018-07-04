14f172107524a105bfb15a38e872d0c2a76bf0ab
Clint Jang <Jang.wangsu@gmail.com>
2018-07-04 18:34:52 +0900

    ➕ add file

---
+++ JWSTimer/JWSTimer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JWSTimer
+//
+//  Created by Clint on 2018. 7. 4..
+//  Copyright © 2018년 clint. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JWSTimer/JWSTimer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JWSTimer
+//
+//  Created by Clint on 2018. 7. 4..
+//  Copyright © 2018년 clint. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
